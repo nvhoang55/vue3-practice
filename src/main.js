8ba1aced304a6e6b6662a7cb3537ea1dd88434c2
nvhoang55 <42000591+nvhoang55@users.noreply.github.com>
2021-12-25 15:44:10 +0700

    feat: set up Firebase, call API from Firestore for CardList data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import {createApp} from "vue";
 import App from "./App.vue";
 import router from "./router";
+//Firebase
+import "./Firebase/setup";
+
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import "./helper.js";
